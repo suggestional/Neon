0bf2451bc5c8bef04a880d2c011c4233fb5d3e35
suggestional <19302010065@fudan.edu.cn>
2022-04-07 19:44:46 +0800

    modify UI in home screen

---
+++ src/views/SettingsPage.vue
@@ -0,0 +1,46 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>设置</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content>
+
+      <ion-list>
+        <ion-item button @click="buttonClick()" detail>
+          <ion-label>
+            设置1
+          </ion-label>
+        </ion-item>
+        <ion-item button @click="buttonClick()" detail>
+  <!--        <ion-icon :icon="walk" slot="start"></ion-icon>-->
+          <ion-label>
+            设置2
+          </ion-label>
+        </ion-item>
+        <ion-item button @click="buttonClick()" detail>
+          <ion-label>
+            设置3
+          </ion-label>
+        </ion-item>
+        <ion-item button @click="buttonClick()" detail>
+          <ion-label>
+            设置4
+          </ion-label>
+        </ion-item>
+
+      </ion-list>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonList, IonItem, IonContent, IonLabel } from '@ionic/vue';
+
+export default defineComponent({
+  name: 'SettingsPage',
+  components: { IonHeader, IonToolbar, IonTitle, IonList, IonItem, IonLabel, IonContent, IonPage }
+});
+</script>

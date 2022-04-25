2a00bcc194095f7abf93373b15e01b668606827e
Summer-Shen <1563407190@qq.com>
2022-04-26 02:10:45 +0800

    feat: modify routes & imports for router

--- src/router/index.js
+++ src/router/index.js
@@ -1,43 +1,53 @@
-import { createRouter, createWebHistory } from '@ionic/vue-router';
+import { createRouter, createWebHistory } from "@ionic/vue-router";
 // import { RouteRecordRaw } from 'vue-router';
-import TabsPage from '../views/TabsPage.vue'
+import TabsPage from "../views/TabsPage.vue";
+import LearnPage from "../views/LearnPage.vue";
+import ReviewPage from "../views/ReviewPage.vue";
+import SettingsPage from "../views/SettingsPage.vue";
+import WordbookSelection from "../views/WordbookSelection.vue";
+import LearnWord from "../views/LearnWord.vue";
 
 const routes = [
   {
-    path: '/',
-    redirect: '/tabs/learn'
+    path: "/",
+    redirect: "/tabs/learn",
   },
   {
-    path: '/tabs/',
+    path: "/tabs/",
     component: TabsPage,
     children: [
       {
-        path: '',
-        redirect: '/tabs/learn'
-      },
-      // {
-      //   path: 'review',
-      //   component: () => import('@/views/ReviewPage.vue')
-      // },
-      {
-        path: 'learn',
-        component: () => import('@/views/LearnPage.vue')
+        path: "",
+        redirect: "/tabs/learn",
       },
       {
-        path: 'settings',
-        component: () => import('@/views/SettingsPage.vue')
-      }
-    ]
+        path: "learn",
+        component: LearnPage,
+      },
+      {
+        path: "review",
+        component: ReviewPage,
+      },
+      {
+        path: "settings",
+        component: SettingsPage,
+      },
+    ],
   },
   {
-    path: '/tabs/learnWord',
-    component: () => import('@/views/LearnWord.vue')
-  }
-]
+    path: "/wordbook-selection",
+    component: WordbookSelection,
+  },
+  {
+    // TODO: add params for dynamic route matching
+    path: "/learn-word",
+    component: LearnWord,
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+export default router;

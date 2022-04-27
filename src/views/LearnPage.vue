1bae23760e1914ca5ca4f05c859fbd4f4dfd90a7
Summer-Shen <1563407190@qq.com>
2022-04-27 22:11:55 +0800

    feat: add links to LearnPage cards

--- src/views/LearnPage.vue
+++ src/views/LearnPage.vue
@@ -5,39 +5,53 @@
         <ion-title>学习</ion-title>
       </ion-toolbar>
     </ion-header>
-  <ion-content fullscreen>
-    <ion-card button @click="buttonClick()">
-      <ion-card-header>
-        <ion-card-title>切换词书</ion-card-title>
-      </ion-card-header>
-    </ion-card>
-    <ion-card button @click="buttonClick()">
-      <ion-card-header>
-        <ion-card-title>开始学习</ion-card-title>
-      </ion-card-header>
-    </ion-card>
-    <ion-card button @click="buttonClick()">
-      <ion-card-header>
-        <ion-card-title>开始复习</ion-card-title>
-      </ion-card-header>
-    </ion-card>
-  </ion-content>
-
-
+    <ion-content fullscreen>
+      <ion-card button href="/wordbook-selection">
+        <ion-card-header>
+          <ion-card-title>切换词书</ion-card-title>
+        </ion-card-header>
+      </ion-card>
+      <!-- TODO: link to units & actual words -->
+      <ion-card button href="/learn-word">
+        <ion-card-header>
+          <ion-card-title>开始学习</ion-card-title>
+        </ion-card-header>
+      </ion-card>
+      <ion-card button href="/tabs/review">
+        <ion-card-header>
+          <ion-card-title>开始复习</ion-card-title>
+        </ion-card-header>
+      </ion-card>
+    </ion-content>
   </ion-page>
-
 </template>
 
 
 
 <script>
-import { defineComponent } from 'vue';
-import { IonPage, IonHeader, IonToolbar, IonContent, IonCard, IonCardHeader, IonCardTitle, IonTitle } from '@ionic/vue';
+import { defineComponent } from "vue";
+import {
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonContent,
+  IonCard,
+  IonCardHeader,
+  IonCardTitle,
+  IonTitle,
+} from "@ionic/vue";
 
 export default defineComponent({
-  name: 'LearnPage',
-  components: { IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardHeader, IonCardTitle, IonPage }
+  name: "LearnPage",
+  components: {
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonCard,
+    IonCardHeader,
+    IonCardTitle,
+    IonPage,
+  },
 });
 </script>
-
-
